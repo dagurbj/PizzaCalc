d5df89a4234682feeb23d9d681826ce9adfaf34b
Dagur Bjarnason <dbj@netcompany.com>
2024-02-01 11:01:39 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,28 @@
+const CACHE_NAME = 'my-app-cache-v2'; // Increment this version as you update your app
 self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open('my-app-cache').then((cache) => {
+        caches.open(CACHE_NAME).then((cache) => {
             return cache.addAll([
                 '/',
                 '/index.html',
-                '/static/css/main.2d8189c2.css',
-                '/static/js/main.0d04e169.js'
+                '/static/css/main.2d8189c2.css', // Update these paths if they change
+                '/static/js/main.0d04e169.js'    // Update these paths if they change
                 // Add other assets you want to cache
             ]);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.filter((cacheName) => {
+                    return cacheName !== CACHE_NAME;
+                }).map((cacheName) => {
+                    return caches.delete(cacheName);
+                })
+            );
         })
     );
 });
